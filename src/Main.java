257bac88efa6cbc107d9aaaa1778c5a28f9516fa
ekatevundt <ekatevundt@yandex.ru>
2022-08-24 16:27:15 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+class Main {
+            public static void main(String[] args){
+                System.out.println("Max rating " + Singer.maxRating);
+                Singer singer = new Singer("petya",8);
+                singer.sing ("Good afternoon");
+                System.out.println(singer);
+                A.x = 5;
+                System.out.println("Max rating " + Singer.maxRating);
+                Singer singer1 = new Singer("anya", 15, 4);
+                System.out.println(singer1);
+
+
+
+
+
+
+
+            }
+        }
+
+
+
